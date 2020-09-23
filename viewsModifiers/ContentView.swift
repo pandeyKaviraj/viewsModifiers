282a5c4a952ab129d92dfe1cd940a90dc15f7d63
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2020-09-23 13:01:29 +0530

    Conditional modifiers in action completed

--- viewsModifiers/ContentView.swift
+++ viewsModifiers/ContentView.swift
@@ -9,11 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var useRedText = false
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.red)
-            .edgesIgnoringSafeArea(.all)
+        Button("Tap me!") {
+            self.useRedText.toggle()
+        }
+        .foregroundColor(useRedText ? .red : .green)
     }
 }
 
