5e828097009dc702bacee46718ee40e595b88a10
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2020-09-23 12:05:10 +0530

    views and modifiers basic structure understood

---
+++ viewsModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  viewsModifiers
+//
+//  Created by KAVIRAJ PANDEY on 22/09/20.
+//  Copyright © 2020 KAVIRAJ PANDEY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.red)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
