7b2cabd3bc8a88c67836c4fb650206db04324d9a
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2020-09-27 09:27:45 +0530

    Created Custom Containers in SwiftUI

--- viewsModifiers/ContentView.swift
+++ viewsModifiers/ContentView.swift
@@ -11,31 +11,35 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Color.gray
-            .frame(width: 300, height: 200)
-        .watermarked(with: "Hacking with Swift")
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-    
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(Color.black)
+        GridStack(rows: 4, column: 4) { row, col in
+            HStack {
+                Image(systemName: "\(row * 4 + col).cicle")
+                Text("R\(row) C\(col)")
+            }
         }
     }
 }
 
-extension View {
-    func watermarked(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
+//Custom Containers
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let column: Int
+    //Take particular note of the let content line – that defines a closure that must be able to accept two integers and return some sort of content we can show.
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach (0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.column, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
     }
 }
+
 
 
 
