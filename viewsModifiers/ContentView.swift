f11e7874776c071aa6df24dad760955919efedd3
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2020-09-30 11:57:11 +0530

    Gridstack view

--- viewsModifiers/ContentView.swift
+++ viewsModifiers/ContentView.swift
@@ -7,38 +7,43 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     
     var body: some View {
-        GridStack(rows: 4, column: 4) { row, col in
+        GridStack(rows: 4, columns: 4) { row, col in
             HStack {
-                Image(systemName: "\(row * 4 + col).cicle")
+                Image(systemName: "\(row * 4 + col).circle")
                 Text("R\(row) C\(col)")
             }
         }
     }
-}
+ }
 
-//Custom Containers
-struct GridStack<Content: View>: View {
+struct GridStack<Content: View>:View {
     let rows: Int
-    let column: Int
-    //Take particular note of the let content line – that defines a closure that must be able to accept two integers and return some sort of content we can show.
+    let columns: Int
     let content: (Int, Int) -> Content
     
     var body: some View {
         VStack {
-            ForEach (0..<rows, id: \.self) { row in
+            ForEach(0..<rows, id: \.self) { row in
                 HStack {
-                    ForEach(0..<self.column, id: \.self) { column in
-                        self.content(row, column)
+                    ForEach(0..<self.columns, id: \.self) { columns in
+                        self.content(row,columns)
                     }
                 }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
 
 
 
